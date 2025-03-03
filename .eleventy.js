d5567a0a3071bb01602d73397c419c562d767c9b
Matias Castillo <177944762+matiascastillodev@users.noreply.github.com>
2025-03-03 20:35:04 -0300

    hm

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
+  eleventyConfig.addPassthroughCopy("src/media");
+  return {
+    dir: {
+      input: "src",
+      output: "public ",
+    },
+  };
 }
