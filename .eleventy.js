32376c9d09ec7accf60c0657a72618a7a42fe885
Matías Castillo <177944762+matiascastillodev@users.noreply.github.com>
2025-03-03 23:11:22 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
-const { DateTime } = require("luxon");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
   });
   return {
     dir: {
